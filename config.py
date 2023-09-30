2f8b164e4ca453f0611b3c05359faf5ff634bd15
brandedrajput <145931805+brandedrajput@users.noreply.github.com>
2023-10-01 04:07:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "6409527937").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/happy_king_roy")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/brandedrajput/Branded_musicbot")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
