5e40cc76a3e866dcf4660e05f3a46a490b770dad
shivamb1208 <151376961+shivamb1208@users.noreply.github.com>
2023-11-20 11:19:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/brandedrajput/Brande
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Branded_sarver1")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Branded_sarver123")
+SUPPORT_CHANNEL = getenv("Official Channel", "https://t.me/Branded_sarver1")
+SUPPORT_GROUP = getenv("Support Group", "https://t.me/Branded_sarver123")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
